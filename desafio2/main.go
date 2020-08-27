824c0649b15db5d02b6ebe9edb5f2ee2c393c54f
David Petter Alves <cfdavidpetter@gmail.com>
2020-08-27 12:36:24 -0300

    Update desafio2

--- desafio2/main.go
+++ desafio2/main.go
@@ -1,10 +1,31 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	oidc "github.com/coreos/go-oidc"
+)
+
 var (
 	clientID     = "app"
 	clientSecret = "790e27c6-457f-4388-8812-effdb5a52505"
 )
 
 func main() {
+	ctx := context.Background()
+
+	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/demo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := oauth2.Config{
+		clientID:     clientID,
+		clientSecret: clientSecret,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  "http://localhost:8081/auth/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "roles"},
+	}
 
 }
