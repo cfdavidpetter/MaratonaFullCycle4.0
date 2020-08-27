9f1bb857adb0dfc4ef786fd7d96b10f86296623f
David Petter Alves <cfdavidpetter@gmail.com>
2020-08-27 18:56:03 -0300

    Update desafio2

---
+++ desafio2/go.mod
@@ -0,0 +1,11 @@
+module keycloak-exemplo
+
+go 1.13
+
+require (
+	github.com/coreos/go-oidc v2.2.1+incompatible // indirect
+	github.com/pquerna/cachecontrol v0.0.0-20200819021114-67c6ae64274f // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
+)
--- desafio2/main.go
+++ desafio2/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
+	"net/http"
 
 	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
 )
 
 var (
@@ -15,17 +18,58 @@ var (
 func main() {
 	ctx := context.Background()
 
-	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/demo")
+	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/App")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	config := oauth2.Config{
-		clientID:     clientID,
-		clientSecret: clientSecret,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
 		Endpoint:     provider.Endpoint(),
 		RedirectURL:  "http://localhost:8081/auth/callback",
 		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "roles"},
 	}
 
+	state := "magica"
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		http.Redirect(writer, request, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	http.HandleFunc("/auth/callback", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("state") != state {
+			http.Error(w, "state did not match", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(w, "failed to exchange token", http.StatusBadRequest)
+			return
+		}
+
+		rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+		if !ok {
+			http.Error(w, "no id_token", http.StatusBadRequest)
+			return
+		}
+
+		resp := struct {
+			OAuth2Token *oauth2.Token
+			RawIDToken  string
+		}{
+			oauth2Token, rawIDToken,
+		}
+
+		data, err := json.MarshalIndent(resp, "", "		")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Write(data)
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
